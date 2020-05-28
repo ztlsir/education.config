fd6bd9025f3dd9677d4082afac2e6d2f6209fdd6
ztlsir <584821699@qq.com>
2020-05-28 12:06:00 +0800

    init project

---
+++ src/main/java/com/ztlsir/config/EducationConfigApplication.java
@@ -0,0 +1,13 @@
+package com.ztlsir.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ztlsir/config/EducationConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztlsir.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
